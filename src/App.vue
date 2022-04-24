28a453734c17f3b93843316bfbe49856bc442f2b
lxl-laocai <liuxilong@88.com>
2022-04-24 22:38:11 +0800

    element-plus 按需引入

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
   <el-button type="primary">Primary</el-button>
   <el-button type="success">Success</el-button>
   <el-button type="info">Info</el-button>
-  <el-button type="warning">Warning</el-button>
 </template>
 
 <script lang="ts">
